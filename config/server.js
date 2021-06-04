d43b66031dabe16c7ccceb732b8826d894926d00
Claudio Lins <69011104+Claudio-Lins@users.noreply.github.com>
2021-06-04 21:52:46 +0100

    Initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '16f358836478a9278f9f16d6ecc02179'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '2d95eb99-24e1-412d-9df5-ffaafe4ce112'
+};
